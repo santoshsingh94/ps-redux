7f4636417390a0ab29689b4d77a917eff0ec8c2c
santoshsingh-94 <ausantoshsingh@gmail.com>
2019-12-13 18:35:10 +0530

    Basic Roting And app configured.

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./home/HomePage";
+import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
+//import PageNotFound from "./PageNotFound";
+//import CoursesPage from "./courses/CoursesPage";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+    </div>
+  );
+}
+
+export default App;
