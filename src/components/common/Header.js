24db769480e7c28315bca0a05b506a9414dd86a5
santoshsingh-94 <ausantoshsingh@gmail.com>
2019-12-13 18:59:13 +0530

    Initial App Structure completed

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -10,6 +10,10 @@ const Header = () => {
         Home
       </NavLink>
       {" | "}
+      <NavLink to="/courses" activeStyle={activeStyle}>
+        Courses
+      </NavLink>
+      {" | "}
       <NavLink to="/about" activeStyle={activeStyle}>
         About
       </NavLink>
